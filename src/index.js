8ff67c9818bd42d74af5c709cdb89fa32698719b
Robert Pliszka <robplisz@gmail.com>
2024-07-13 16:03:12 -0400

    Created additional components in the root file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
 }
+
+const Person = () => <h2>John Doe</h2>;
+const Message = () => <p>This is my message</p>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

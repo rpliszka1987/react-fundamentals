aeb61bf7be732db22b09ec0de820fce1cb8644ac
Robert Pliszka <robplisz@gmail.com>
2024-09-01 14:16:16 -0400

    Added some styles to the Author element

--- src/index.js
+++ src/index.js
@@ -28,7 +28,12 @@ const Title = () => (
   <h2>Hillbilly Elegy: A Memoir of a Family and Culture in Crisis</h2>
 );
 const Author = () => {
-  return <h4>J. D. Vance</h4>;
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  };
+  return <h4 style={inlineHeadingStyles}>J. D. Vance</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

bf967526ee57dd145c4e7569f40d97bf0ede7adf
Robert Pliszka <robplisz@gmail.com>
2024-09-02 19:25:49 -0400

    Added props to pass to Book component

--- src/index.js
+++ src/index.js
@@ -2,26 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const title = 'Hillbilly Elegy: A Memoir of a Family and Culture in Crisis';
+const author = 'J. D. Vance';
+const image = './images/book-1.jpg';
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} image={image} />
+      <Book author={author} title={title} image={image} />
+      <Book author={author} title={title} image={image} />
+      <Book author={author} title={title} image={image} />
     </section>
   );
 };
 
-const Book = () => {
-  const title = 'Hillbilly Elegy: A Memoir of a Family and Culture in Crisis';
-  const author = 'J. D. Vance';
-  const image = './images/book-1.jpg';
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };

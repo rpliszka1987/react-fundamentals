bbecc343af542bc4a86cb39a555e2afd4ec2ac44
Robert Pliszka <robplisz@gmail.com>
2024-09-02 19:50:27 -0400

    added destructing to the Book component props

--- src/index.js
+++ src/index.js
@@ -34,12 +34,12 @@ const BookList = () => {
 };
 
 const Book = (props) => {
-  console.log(props);
+  const {image, title, author} = props;
   return (
     <article className="book">
-      <img src={props.image} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };

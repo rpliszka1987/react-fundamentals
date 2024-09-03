b8f9acf1f7027a40957c5cb67c589b2f683a66b3
Robert Pliszka <robplisz@gmail.com>
2024-09-02 21:36:43 -0400

    Using map method to go through the book object array and insert into page

--- src/index.js
+++ src/index.js
@@ -2,49 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const firstBook = {
-  title: 'Hillbilly Elegy: A Memoir of a Family and Culture in Crisis',
-  author: 'J. D. Vance',
-  image: './images/book-1.jpg',
-};
+const books = [
+  {
+    title: 'Hillbilly Elegy: A Memoir of a Family and Culture in Crisis',
+    author: 'J. D. Vance',
+    image: './images/book-1.jpg',
+  },
 
-const secondBook = {
-  title: 'Linux for Absolute Beginners',
-  author: 'Andrew Warner',
-  image:
-    'https://c.media-amazon.com/images/I/71gvbSzk+IL._UX300__PJku-sticker-v8,TopRight,0,-50_AC_UL254_SR254,254_.jpg',
-};
+  {
+    title: 'Linux for Absolute Beginners',
+    author: 'Andrew Warner',
+    image:
+      'https://c.media-amazon.com/images/I/71gvbSzk+IL._UX300__PJku-sticker-v8,TopRight,0,-50_AC_UL254_SR254,254_.jpg',
+  },
 
-const thirdBook = {
-  title: 'Atomic Habits',
-  author: 'James Clear',
-  image: 'https://c.media-amazon.com/images/I/81YkqyaFVEL._SY466_.jpg',
-};
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    image: 'https://c.media-amazon.com/images/I/81YkqyaFVEL._SY466_.jpg',
+  },
+];
+
+
 
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        image={firstBook.image}
-      />
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        image={secondBook.image}
-      />
-      <Book
-        title={thirdBook.title}
-        author={thirdBook.author}
-        image={thirdBook.image}
-      />
-    </section>
-  );
+  return <section className="booklist">
+    {books.map((book)=> {
+      const {title, author, image} = book;
+      return <Book title={title} author={author} image={image}/>
+    })}
+
+  </section>;
 };
 
 const Book = (props) => {
-  const {image, title, author} = props;
+  const { image, title, author } = props;
   return (
     <article className="book">
       <img src={image} alt={title} />

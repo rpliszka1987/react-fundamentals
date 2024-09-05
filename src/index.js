820229abed1c42cc03a321022f5d2058a1d95ff9
Robert Pliszka <robplisz@gmail.com>
2024-09-05 18:08:01 -0400

    Created form with events on change and on button click

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const books = [
     title: 'Hillbilly Elegy: A Memoir of a Family and Culture in Crisis',
     author: 'J. D. Vance',
     image: './images/book-1.jpg',
-    id: 1
+    id: 1,
   },
 
   {
@@ -15,32 +15,51 @@ const books = [
     author: 'Andrew Warner',
     image:
       'https://c.media-amazon.com/images/I/71gvbSzk+IL._UX300__PJku-sticker-v8,TopRight,0,-50_AC_UL254_SR254,254_.jpg',
-      id: 2
+    id: 2,
   },
 
   {
     title: 'Atomic Habits',
     author: 'James Clear',
     image: 'https://c.media-amazon.com/images/I/81YkqyaFVEL._SY466_.jpg',
-    id: 3
+    id: 3,
   },
 ];
 
-
-
 const BookList = () => {
-  return <section className="booklist">
-    {books.map((book)=> {
-      const {id} = book;
-      return <Book {...book} key={id}/>
-    })}
+  return (
+    <section className="booklist">
+      <EventExamples />
+      {books.map((book) => {
+        const { id } = book;
+        return <Book {...book} key={id} />;
+      })}
+    </section>
+  );
+};
 
-  </section>;
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input');
+  };
+
+  const handleButtonClick = () => {
+    alert('form submitted');
+  };
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input type="text" name="example" onChange={handleFormInput} stype={{ margin: '1rem 0' }} />
+      </form>
+      <button onClick={handleButtonClick}>Click Me</button>
+    </section>
+  );
 };
 
 const Book = (props) => {
   const { image, title, author } = props;
-  console.log(props)
+  console.log(props);
   return (
     <article className="book">
       <img src={image} alt={title} />

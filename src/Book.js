51e9e4969299c67e865c545f7b68b044ad688e9c
Robert Pliszka <robplisz@gmail.com>
2024-09-09 20:17:38 -0400

    Moved the Book component from index to Books.js

---
+++ src/Book.js
@@ -0,0 +1,14 @@
+const Book = (props) => {
+  const { image, title, author } = props;
+
+  return (
+    <article className="book">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+export default Book;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {books} from './books';
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
   return (
@@ -11,19 +12,6 @@ const BookList = () => {
         return <Book {...book} key={id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { image, title, author } = props;
-
-  return (
-    <article className="book">
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-
-      <h4>{author}</h4>
-    </article>
   );
 };
 
